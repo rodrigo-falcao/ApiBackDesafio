400a9eae06f869839ce55a3c8d1637b451a6d19f
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-08-13 11:38:39 -0300

    inclusão do arquivo routes e alteração no index.js

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger/swagger_output.json');
 const swaggeroptions = { customCssUrl: '/swagger-ui.css' }
-
-const usersRouter = require('./routes/users');
-
+const routes = require('./src/routes');
 const app = express();
 require('dotenv').config();
 
+
+// configuração do express
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +18,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { /* #swagger.igoner = true */ res.redirect('/doc'); });
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggeroptions));
-app.use('/users', usersRouter);
 
+// Rotas da documentação do swagger
+if(process.env.NODE_ENV !== 'test'){
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => { /* #swagger.igoner = true */ res.redirect('/doc'); });
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggeroptions));
+}
+
+
+// Rotas das APIs
+routes(app);
+
+
+// inicialização do servidor e indicação da porta
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
